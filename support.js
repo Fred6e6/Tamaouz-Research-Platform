7a19e3144e9c3908f5c62c23bbbfd175f33b041d
Fred6e6 <f2024ali22@gmail.com>
2026-09-13 20:09:31 +0300

    Connect support page to standalone Complaints API

--- support.js
+++ support.js
@@ -1,4 +1,4 @@
-const API_URL='https://script.google.com/macros/s/AKfycbw4GeV2ZDrYcPq8fYSuHhWtold8OUseRLU_mXBOztprFH_L4Q63ZiDuQYOwtZ9GaEz2/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbz_1vqQxiffArbl0OC4Ff0MmMvTlUbZ_p1yaqKOd2y-OKHnaB1NXIqQ1IAH7v3CBzHp/exec';
 let lang=localStorage.getItem('tamaouz_language')||'ar';
 const form=document.getElementById('supportForm');
 const msg=document.getElementById('formMessage');
